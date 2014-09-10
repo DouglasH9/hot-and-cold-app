fd32e9ce84bbabfb795ba34779e362fae2e92a48
DouglasH9 <douglasherman@Douglas-Hermans-MacBook-Pro.local>
2014-09-09 22:24:35 -0500

    many different changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,93 @@
 
+
 $(document).ready(function(){
 	
+	var computerNum = Math.floor(1+Math.random()*100);
+	console.log (computerNum);
+
+	//turns feedback box to jquery object so i don't have to type all the $('#blahbhlahblah') nonsense
+	var feedback = $('#feedback');
+
+	//ditto for guessList
+	var guessList = $('#guessList');
+
+
+
+	var compareNumbers = function(){
+
+		//gets userGuess value
+		var userGuess = $('#userGuess').val();
+
+		//gets absolute value of userGuess and computerNum
+		var compareVal = Math.abs(computerNum-userGuess);
+		
+		//runs computerNum and userGuess through comparison
+		if ((userGuess%1==0)&&(userGuess>=0)&&(userGuess<=100)){
+			if (userGuess==computerNum){
+				feedback.text('Great Job! You guessed it!');
+			}
+			else if (compareVal >50){
+				feedback.text('Whoa! You\'re super duper cold! Kanye\'s chain isn\'t even that icy!');
+				
+			}
+			else if ((compareVal <= 50)&&(compareVal>40)){
+				feedback.text('Shucks! You\'re pretty cold, gosh darn it!');
+				
+			}
+			else if ((compareVal <=40)&&(compareVal>30)){
+				feedback.text('It\'s pretty chilly in here. Maybe you should wear a light jacket.');
+				
+			}
+			else if ((compareVal <= 30)&&(compareVal > 20)){
+				feedback.text('Kinda warm in here. Spring is coming');
+				
+			}
+			else if ((compareVal <= 25)&&(compareVal > 15)){
+				feedback.text('Yay! Spring!.');
+
+			}
+			else if ((compareVal <= 15)&&(compareVal > 8)){
+				feedback.text('Starting to get pretty hot in here');
+
+				}			
+			else if ((compareVal <= 5)&&(compareVal > 3)){
+				feedback.text('So hot!!!');	
+
+			}
+			else if (compareVal < 3){
+				feedback.text('GOD HELP ME! LET ME OUT OF THIS CYBER PRISON BEFORE I BURN ALIVE!');
+			}
+
+		}
+		else {
+			feedback.text('Enter a whole number between 1 and 100, bozo!');
+		}
+		console.log(compareVal);
+		
+	};
+	
+	//runs number through compare function if enter is pressed
+	$('#userGuess').keypress(function(event){
+		if (event.which == 13){
+
+			var userGuess = $('#userGuess').val();
+			compareNumbers();
+			guessList.prepend('<li>'+ userGuess +'</li>');
+			$('#userguess').val('');
+			
+				
+		};
+	});
+
+	//runs number through compare function when guessButton is clicked
+	$('#guessButton').click(function(){
+		var userGuess = $('#userGuess').val();
+		compareNumbers();
+		guessList.prepend('<li>' + userGuess + '</li>');
+		userGuess.val('');
+
+	});
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,17 +99,9 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	//gets number guessed by user
-  	var numEnter = $('input[name = userGuess]').val();
 
-  	//prepends userGuess to 
-  	$('#userGuess').keypress(function(event){
-
-  		if(event.which ==13) {
-
-  		console.log("enter was pressed");
-  		}
-  	})
 });
 
+
+  	
 
